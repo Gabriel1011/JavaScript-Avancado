0bc104123d523f3f350ecbdd9e477688b2e0dddf
Gabriel1011 <38117500+Gabriel1011@users.noreply.github.com>
2018-09-19 22:00:30 -0300

    Negociação

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,15 +1,27 @@
 class NegociacaoController {
 
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
     adiciona(event) {
         event.preventDefault();
-        
 
-        let inputData = document.querySelector('#data');
-        let inputQuantidade = document.querySelector('#quantidade');
-        let inputValor = document.querySelector('#valor');
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map((item, indice) => item - indice % 2)
+        );
 
-        console.log(inputData);
-        console.log(inputQuantidade);
-        console.log(inputValor);
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
     }
 }
